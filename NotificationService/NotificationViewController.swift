1764bccc186821015f549091ee52669ff80538d0
Chetan <chetan.a@clevertap.com>
2023-02-09 14:56:56 +0530

    changes by Chetan

---
+++ NotificationService/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  NotificationService
+//
+//  Created by Chetan Ashtivkar on 09/08/22.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
---
+++ ct_push/DeepLinkScreen.swift
@@ -0,0 +1,20 @@
+//
+//  DeepLinkScreen.swift
+//  ct_push
+//
+//  Created by Chetan Ashtivkar on 23/08/22.
+//
+
+import SwiftUI
+
+struct DeepLinkScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DeepLinkScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        DeepLinkScreen()
+    }
+}
